376fd767a832f987d435c811e85ea85eb16db028
kkumaki12 <k.kumaki12@gmail.com>
2024-07-08 20:14:38 +0900

    Todosテーブルマイグレーション

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module kkumaki12/go-todo
 
 go 1.22.0
+
+require github.com/mattn/go-sqlite3 v1.14.22 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+var db *sql.DB
+
+func initDb() {
+	var err error
+	db, err = sql.Open("sqlite3", "./todos.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmd := `CREATE TABLE IF NOT EXISTS todos (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		content TEXT,
+		created_at DATETIME
+	);`
+	_, err = db.Exec(cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	hello := []byte("Hello, World!")
@@ -14,6 +36,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	initDb()
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
 	http.ListenAndServe(":8080", nil)
